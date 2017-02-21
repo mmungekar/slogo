5d8d582861445d3cd2a3df852ad3760b7f73eeab
Juan Philippe <jap87@duke.edu>
2017-02-21 01:16:18 -0500

    refactored, added parameter interface

--- src/back_end/CommandBuilder.java
+++ src/back_end/CommandBuilder.java
@@ -1,9 +1,16 @@
 package back_end;
 
+import commands.Command;
+
 public class CommandBuilder
 {
 	public CommandBuilder()
 	{
 		
+	}
+
+	public Command getCommand(String firstWord) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
--- src/back_end/StringInterpreter.java
+++ src/back_end/StringInterpreter.java
@@ -16,16 +16,10 @@ public class StringInterpreter
 	
 	public Command interpret(String commandString)
 	{
-		return parseFirstWord(commandString);
-	}
-
-	private Command parseFirstWord(String commandString)
-	{
 		scanner = new Scanner(commandString);
-		//ugly if statements go "hidden away" here, part of internal API; to extend commands, add a new line here
-		//example: if first word = fd command, return createNewFDCommand(), which uses the scanner to parse the next parameters,
-		// and call an error if the parameters don't make sense, or returns a fdcommand object with the correct parameters
-		return null;
+		Command command = commandBuilder.getCommand(scanner.next());
+		
+		command.setParameters(scanner.nextLine());
+		return command;
 	}
-
 }
