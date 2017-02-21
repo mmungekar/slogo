552c34c98c008016090ba692c531d99beeeba1c0
Miguel Anderson <mra21@duke.edu>
2017-02-21 16:16:25 -0500

    History works, scrolls to bottom and allows for reselecting

--- src/front_end/Terminal.java
+++ src/front_end/Terminal.java
@@ -1,26 +1,39 @@
 package front_end;
 
 //http://codereview.stackexchange.com/questions/52197/console-component-in-javafx
+///http://docs.oracle.com/javafx/2/ui_controls/list-view.htm
 
 import java.util.ArrayList;
 import java.util.List;
 
 import java.util.function.Consumer;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.input.KeyEvent;
-
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 public class Terminal {
 	private TextArea output = new TextArea();
 	private TextField input = new TextField();
+	private ListView<String> historyView = new ListView<String>();
+
+	public static final int CONSOLE_WIDTH = 500;
 
 	private VBox console;
 
-	protected final List<String> history = new ArrayList<>();
 	protected int historyPointer = 0;
-	
+
+	public static final ObservableList<String> history = FXCollections.observableArrayList();
+
 	public final static String EMPTY_STRING = "";
 
 	private Consumer<String> onMessageReceivedHandler;
@@ -28,6 +41,42 @@ public class Terminal {
 	public Terminal() {
 		output.setEditable(false);
 		console = new VBox();
+		setupInput();
+		setupHistoryView();
+		console.getChildren().addAll(historyView, input);
+	}
+
+	private void setupHistoryView() {
+		historyView.setItems(history);
+		historyView.setPrefWidth(CONSOLE_WIDTH);
+		historyView.setPrefHeight(175);		
+		setMouseActionOnListView();
+
+	}
+
+	private void setMouseActionOnListView() {
+		// http://stackoverflow.com/questions/23622703/deselect-an-item-on-an-javafx-listview-on-click
+		historyView.setCellFactory(lv -> {
+            ListCell<String> cell = new ListCell<>();
+            cell.textProperty().bind(cell.itemProperty());
+            cell.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> {
+                historyView.requestFocus();
+                if (! cell.isEmpty()) {
+                    int index = cell.getIndex();
+                    if (!historyView.getSelectionModel().getSelectedIndices().contains(index)) {
+                    	historyView.getSelectionModel().select(index);
+                    	input.setText(cell.getItem());
+    				    submitInput();
+                    	historyView.getSelectionModel().clearSelection(index);
+                    }
+                    event.consume();
+                }
+            });
+            return cell ;
+        });
+	}
+
+	private void setupInput() {
 		input.addEventHandler(KeyEvent.KEY_RELEASED, keyEvent -> {
 			switch (keyEvent.getCode()) {
 			case ENTER:
@@ -53,21 +102,22 @@ public class Terminal {
 				break;
 			}
 		});
-
-		console.getChildren().addAll(input, output);
+		input.setPrefWidth(CONSOLE_WIDTH);
 	}
 
 	private void loadHistoryIntoTextInput() {
+		System.out.println(historyPointer);
 		input.setText(history.get(historyPointer));
 		input.selectAll();
 	}
 
 	void submitInput() {
 		String text = input.getText();
-		if(!text.equals(EMPTY_STRING)){
-			output.appendText(text + System.lineSeparator());
+		historyView.scrollTo(history.size()-1);
+		if (!text.equals(EMPTY_STRING)) {
+			// output.appendText(text + System.lineSeparator());
 			history.add(text);
-			historyPointer++;
+			historyPointer = history.size();
 			if (onMessageReceivedHandler != null) {
 				onMessageReceivedHandler.accept(text);
 			}
