0b0a37f14c92f8b29ab960624d65154fccc694bb
Mina Mungekar <mem94@duke.edu>
2017-02-22 22:34:13 -0500

    Added methods to parse and store user input and type

---
+++ src/back_end/Input.java
@@ -0,0 +1,13 @@
+package back_end;
+
+public class Input {
+	private Object myParameter;
+	private String myType;
+	public Input(){
+		
+	}
+	public Input(String parameter, String type){
+		myParameter = parameter;
+		myType = type;
+	}
+}
