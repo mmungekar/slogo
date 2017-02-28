8c6fbb9dc7a880f4e44c92a5853d0db235a908c3
Mina Mungekar <mem94@duke.edu>
2017-02-28 17:30:10 -0500

    new file

--- src/back_end/ExpressionTreeNode.java
+++ src/back_end/ExpressionTreeNode.java
@@ -1,9 +1,5 @@
 package back_end;
-<<<<<<< HEAD
-import java.lang.reflect.InvocationTargetException;
-=======
 	
->>>>>>> branch 'master' of git@coursework.cs.duke.edu:CompSci308_2017Spring/slogo_team16.git
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -11,14 +7,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.concurrent.Callable;
-<<<<<<< HEAD
-import commands.CommandInterface;
-=======
 
 import back_end.commands.CommandInterface;
 import back_end.constant.Constant;
 
->>>>>>> branch 'master' of git@coursework.cs.duke.edu:CompSci308_2017Spring/slogo_team16.git
 /**
  * Constructs the nodes of the expression tree, which will contain either
  * commands, operators, or integer values (hence, type Object)
@@ -31,53 +23,8 @@ public class ExpressionTreeNode {
 	private double mValue;
 	private ExpressionTreeNode myParent;
 	private List<ExpressionTreeNode> myChildren;
-<<<<<<< HEAD
-	private int requiredChildNumber;
-	private boolean isExecuted;
-	/**
-	 * If no parameters are specified in the constructor, the node is
-	 * initialized without a parent node or contents
-	 * 
-	 * @throws InvocationTargetException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 * @throws SecurityException
-	 * @throws NoSuchMethodException
-	 * @throws ClassNotFoundException
-	 * @throws InstantiationException
-	 * @throws LibraryLookUpException
-	 * @throws UnrecognizedCommandException
-	 */
-	public ExpressionTreeNode() throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException,
-			UnrecognizedCommandException, LibraryLookUpException {
-		this(null, null);
-	}
-	/**
-	 * There are multiple ways to construct the node- if the children nodes are
-	 * unknown, a lone node with a single value is created
-	 * 
-	 * @param x
-	 *            specifies the contents of the node
-	 * @throws InvocationTargetException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 * @throws SecurityException
-	 * @throws NoSuchMethodException
-	 * @throws ClassNotFoundException
-	 * @throws InstantiationException
-	 * @throws LibraryLookUpException
-	 * @throws UnrecognizedCommandException
-	 */
-	public ExpressionTreeNode(Input x) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException,
-			UnrecognizedCommandException, LibraryLookUpException {
-		this(x, null);
-	}
-=======
 	private boolean mExecuted;
 
->>>>>>> branch 'master' of git@coursework.cs.duke.edu:CompSci308_2017Spring/slogo_team16.git
 	/**
 	 * If the parent node is specified, it is included in the constructor
 	 * 
@@ -108,16 +55,13 @@ public class ExpressionTreeNode {
 			break;
 		}
 	}
-<<<<<<< HEAD
-	private void addChild(ExpressionTreeNode child) {
-=======
 
 	public void addChild(ExpressionTreeNode child) {
->>>>>>> branch 'master' of git@coursework.cs.duke.edu:CompSci308_2017Spring/slogo_team16.git
 		if (this != null) {
 			this.myChildren.add(child);
 		}
 	}
+
 	/**
 	 * Returns the parent of the current node
 	 * 
@@ -126,13 +70,6 @@ public class ExpressionTreeNode {
 	public ExpressionTreeNode getParent() {
 		return this.myParent;
 	}
-<<<<<<< HEAD
-	/**
-	 * Returns the content of the current node
-	 * 
-	 * @return the double/string/command the node is storing
-	 */
-=======
 	
 	public void setParent(ExpressionTreeNode parent){
 		myParent = parent;
@@ -150,77 +87,11 @@ public class ExpressionTreeNode {
 		return mValue;
 	}
 
->>>>>>> branch 'master' of git@coursework.cs.duke.edu:CompSci308_2017Spring/slogo_team16.git
 	public Input getInput() {
 		return mInput;
 	}
-<<<<<<< HEAD
-	/**
-	 * Returns the children of the current node
-	 * 
-	 * @return the list of children of the current node
-	 */
-	public List<ExpressionTreeNode> getChildren() {
-		return this.myChildren;
-	}
-	/**
-	 * Returns the required number of children of the current node (i.e. for a
-	 * command)
-	 * 
-	 * @return the necessary number of children of the current node
-	 */
-	public int getRequiredChildNumber() {
-		return this.requiredChildNumber;
-	}
-	/**
-	 * The next two methods cast an input string to a corresponding object (can
-	 * be moved to the Input class) NOTE TO SELF: THINK ABOUT ALTERNATIVES TO
-	 * IF-ELSE STATEMENTS BELOW
-	 * 
-	 * @return
-	 * @throws InvocationTargetException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 * @throws SecurityException
-	 * @throws NoSuchMethodException
-	 * @throws InstantiationException
-	 * @throws LibraryLookUpException
-	 * @throws UnrecognizedCommandException
-	 */
-	private void checkContents(Input x) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException,
-			UnrecognizedCommandException, LibraryLookUpException {
-		if (x.getType().equals("Constant")) {
-			myValue = Double.parseDouble(x.getParameter());
-			requiredChildNumber = commandLib.getNumParam(x.getParameter());
-		} else if (x.getType().equals("Command")) {
-			myCommand = translateInput(x);
-		}
-	}
-	/**
-	 * This method, only called when the Input object contains a command names,
-	 * casts that string name to a command
-	 * 
-	 * @param Input
-	 *            x is the input taken in
-	 * @return CommandInterface returns an instance of the command from its name
-	 * @throws ClassNotFoundException
-	 * @throws InstantiationException
-	 * @throws IllegalAccessException
-	 * @throws LibraryLookUpException
-	 * @throws UnrecognizedCommandException
-	 */
-	public CommandInterface translateInput(Input x) throws ClassNotFoundException, InstantiationException,
-			IllegalAccessException, UnrecognizedCommandException, LibraryLookUpException {
-		CommandInterface command = commandLib.getCommand(x.getParameter());
-		return command;
-	}
-	public void setSatisfied() {
-		isExecuted = true;
-=======
 
 	public void setExecuted() {
 		mExecuted = true;
->>>>>>> branch 'master' of git@coursework.cs.duke.edu:CompSci308_2017Spring/slogo_team16.git
 	}
 }
