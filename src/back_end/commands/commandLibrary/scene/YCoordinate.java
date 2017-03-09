d0ac24408133de82477bfa04dba08705823bab6c
Miguel Anderson <mra21@duke.edu>
2017-03-09 04:38:34 -0500

    Small refactoring and fixes. Added heading command, eliminated duplicated code and unused references.

--- src/back_end/commands/commandLibrary/scene/YCoordinate.java
+++ src/back_end/commands/commandLibrary/scene/YCoordinate.java
@@ -1,7 +1,5 @@
 package back_end.commands.commandLibrary.scene;
 
-import back_end.model.expressiontree.ExpressionTree;
-import back_end.model.expressiontree.Oxygen;
 import back_end.model.scene.Model;
 import back_end.commands.commandLibrary.SimpleParameterCommand;
 import back_end.interfaces.CommandInterface;
