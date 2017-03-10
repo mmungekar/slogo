2aac58cfd0f8b044e17b62af09900e42477e296c
Miguel Anderson <mra21@duke.edu>
2017-03-10 10:12:20 -0500

    Slight refactoring, extracting of constants and removing old code

--- src/main/ControllerMaster.java
+++ src/main/ControllerMaster.java
@@ -3,23 +3,32 @@ package main;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class ControllerMaster {
 
-	public static final String TAB_SIZE = "                  ";
+	private static final String APP_TITLE = "SLogo Team 16";
+	private static final int APP_WINDOW_WIDTH = 800;
+	private static final int APP_WINDOW_HEIGHT = 900;
+	private static final Color TAB_BACKGROUND_COLOR = Color.AQUA;
+	public static final String TAB_TITLE_FORMAT = "Tab %d";
 
 	private TabPane tabPane;
+	private Integer tabNumber;
 	
 	public ControllerMaster()
 	{
 		tabPane = new TabPane();
+		tabNumber = 0;
 	}
 
 	private void createNewGame() {
-		Tab newTab = new Tab(TAB_SIZE);
+		tabNumber ++;
+		Tab newTab = new Tab(String.format(TAB_TITLE_FORMAT, tabNumber));
 		Controller newRuntime = new Controller();
 		tabPane.getTabs().add(newTab);
 		newRuntime.start(newTab);
@@ -27,15 +36,20 @@ public class ControllerMaster {
 		tabPane.getSelectionModel().select(newTab);
 	}
 
+	/**
+	 * Sets up scene and the BorderPane that is used to organize the GUI
+	 * @param primaryStage
+	 */
 	public void start(Stage primaryStage)
 	{
 		BorderPane myRoot = new BorderPane();
-		Scene scene = new Scene(myRoot, 900, 800, Color.BLUE);
+		Scene scene = new Scene(myRoot, APP_WINDOW_HEIGHT, APP_WINDOW_WIDTH);
 		myRoot.setTop(tabPane);
+		myRoot.setBackground(new Background(new BackgroundFill(TAB_BACKGROUND_COLOR, null, null)));
 		
 		createNewGame();
 		
-		primaryStage.setTitle("SLogo Team 16");
+		primaryStage.setTitle(APP_TITLE);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
