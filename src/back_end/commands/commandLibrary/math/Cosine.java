fa837b9df1afae5fdbdcdd7038eed32ac7153e22
Miguel Anderson <mra21@duke.edu>
2017-03-23 19:15:22 -0400

    Picked masterpiece files, added comments

--- src/back_end/commands/commandLibrary/math/Cosine.java
+++ src/back_end/commands/commandLibrary/math/Cosine.java
@@ -1,15 +1,17 @@
+// This entire file is part of my masterpiece.
+// MIGUEL ANDERSON
+
+// This is an example of how OneInputMathCommand is implemented
+
 package back_end.commands.commandLibrary.math;
 
 import java.util.function.DoubleUnaryOperator;
 
 import back_end.interfaces.CommandInterface;
 
-public class Cosine extends OneInputMathCommand implements CommandInterface{
-
+public class Cosine extends OneInputMathCommand implements CommandInterface {
 	@Override
 	protected DoubleUnaryOperator getOperation() {
 		return input -> Math.cos(Math.toRadians(input));
 	}
-
-
 }
